6bb4ca1914c73c998f95d6712fd32197be7adaeb
David Solano <davsolano@estudiantec.cr>
2026-09-02 00:41:46 -0600

    Se trabajó en la aplicacion web

---
+++ TP1/src/tareaProgramada1/tareaProgramada1/Services/EmpleadoService.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using tareaProgramada1.Models;
+
+namespace tareaProgramada1.Services
+{
+    public class EmpleadoService
+    {
+        private readonly string _connectionString;
+
+        public EmpleadoService(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        // Método para listar empleados usando el SP
+        public List<Empleado> ListarEmpleados()
+        {
+            var empleados = new List<Empleado>();
+
+            using (var conn = new SqlConnection(_connectionString))
+            {
+                conn.Open();
+                using (var cmd = new SqlCommand("sp_ListarEmpleados", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            empleados.Add(new Empleado
+                            {
+                                Id = reader.GetInt32(0),
+                                Nombre = reader.GetString(1),
+                                Salario = reader.GetDecimal(2)
+                            });
+                        }
+                    }
+                }
+            }
+
+            return empleados;
+        }
+    }
+}
