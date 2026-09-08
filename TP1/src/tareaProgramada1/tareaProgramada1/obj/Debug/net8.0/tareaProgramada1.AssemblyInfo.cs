00599558b0d0b2192fa238aa1bbb7aa5e3871b36
David Solano <davsolano@estudiantec.cr>
2026-09-08 01:03:09 -0600

    Script sp listado

--- TP1/src/tareaProgramada1/tareaProgramada1/obj/Debug/net8.0/tareaProgramada1.AssemblyInfo.cs
+++ TP1/src/tareaProgramada1/tareaProgramada1/obj/Debug/net8.0/tareaProgramada1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tareaProgramada1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bb4ca1914c73c998f95d6712fd32197be7adaeb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b93673b1adde37f476150f3d039bf3297c705de9")]
 [assembly: System.Reflection.AssemblyProductAttribute("tareaProgramada1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tareaProgramada1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
